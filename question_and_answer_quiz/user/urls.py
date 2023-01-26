7204e50bdc73b27fbd085ee193209e159018b2a6
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-01-25 23:49:08 -0300

    fix: remove account import

--- question_and_answer_quiz/user/urls.py
+++ question_and_answer_quiz/user/urls.py
@@ -1,19 +1,8 @@
-from django.urls import re_path, path
+from django.urls import path
 
-from user.views.account import CreateUserViewSet, ChangePasswordView
 from user.views.auth import LoginView, RefreshTokenView
 
 urlpatterns = [
-    re_path(
-        r"account/create/(?P<user_type>admin|player+)/$",
-        CreateUserViewSet.as_view({"post": "create"}),
-        name="Create admin or player user",
-    ),
-    path(
-        "account/change-password",
-        ChangePasswordView.as_view(),
-        name="Change User Password",
-    ),
     path("auth/login/", LoginView.as_view(), name="User Login"),
     path(
         "auth/refresh/",
