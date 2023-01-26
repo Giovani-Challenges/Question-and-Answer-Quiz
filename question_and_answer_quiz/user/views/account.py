94ee70369c26b487267252b58393936735f63620
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-01-26 15:46:51 -0300

    feat: account view

--- question_and_answer_quiz/user/views/account.py
+++ question_and_answer_quiz/user/views/account.py
@@ -1,12 +1,14 @@
 from typing import Literal
+
 from django.forms.models import model_to_dict
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from user.models import UserModel
-from user.templates.callback import UserCallbackSerializer
-from user.templates.create import CreateUserSerializer
+from user.templates.account import CreateUserSerializer, UserCallbackSerializer
+from utils.permissions import IsAnyUser
 
 
 class CreateUserViewSet(GenericViewSet):
@@ -27,6 +29,9 @@ class CreateUserViewSet(GenericViewSet):
         return Response(data=callback.data, status=status.HTTP_201_CREATED)
 
 
-class CreatePlayerView(GenericViewSet):
-    serializer_class = CreateUserSerializer
-    permission_classes = [AllowAny]
+class ChangePasswordView(APIView):
+    permission_classes = [IsAnyUser]
+
+    def post(self, request):
+        print(request.data)
+        return Response(data={}, status=status.HTTP_200_OK)
