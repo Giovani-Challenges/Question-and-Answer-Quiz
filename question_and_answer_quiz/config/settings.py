9def4f430446b1c610933acfbd2fc207c5c85fc0
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-01-26 15:35:43 -0300

    fix: config load

--- question_and_answer_quiz/config/settings.py
+++ question_and_answer_quiz/config/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.getenv("ENV", "LOCAL") == "LOCAL":
+    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
