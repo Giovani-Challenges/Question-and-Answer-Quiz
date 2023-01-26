dc37f68c710b78736b5b95d5854fd366b1efe91d
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-01-25 23:16:03 -0300

    feat: django permissions

---
+++ question_and_answer_quiz/utils/permissions.py
@@ -0,0 +1,71 @@
+from typing import List, Optional, Tuple
+from rest_framework.permissions import BasePermission
+from user.models.user import UserModel
+from user.views.errors import UnprocessableEntity, Unauthorized
+from utils.decorators import inject
+from utils.jwt import JwtToken
+from jose.exceptions import ExpiredSignatureError, JWTError
+
+
+@inject(JwtToken)
+class BaseBearer(BasePermission):
+    jwttoken: JwtToken
+
+    def get_authorization_content(self, authorization: str) -> Tuple[str, str]:
+        _type, token, *_ = authorization.split(" ")
+        return _type, token
+
+    def get_authorization_from_header(self, header) -> Optional[dict]:
+        return header.get("Authorization")
+
+    def contain_permission(self, permissions: List[str], payload: dict):
+        payload_permissions = payload.get("permissions", [])
+        for permission in payload_permissions:
+            if not permission in permissions:
+                return False
+        return True
+
+    def is_bearer(self, _type: str):
+        return _type == "Bearer"
+
+    def has_permission(self, request, view, **kwargs) -> bool:
+        authorization = self.get_authorization_from_header(request.headers)
+        if not authorization:
+            raise UnprocessableEntity(
+                detail="You must provide a Authorization header with Bearer token"
+            )
+        _type, token = self.get_authorization_content(authorization)
+        if not self.is_bearer(_type):
+            raise UnprocessableEntity(
+                detail="You must provide a Authorization header with Bearer token"
+            )
+        try:
+            payload = self.jwttoken.decode(token)
+        except ExpiredSignatureError:
+            raise Unauthorized(  # pylint: disable=W0707
+                detail="Expired token. Please refresh your token or make the login again"
+            )
+        except JWTError:
+            raise Unauthorized(  # pylint: disable=W0707
+                detail="Invalid token. Please do the login in our api or register your account"
+            )
+        if not self.contain_permission(
+            permissions=kwargs.get("permissions", []), payload=payload
+        ):
+            raise Unauthorized(detail="You don't have permission to use this endpoint")
+        request.user = payload.get("user")
+        return True
+
+
+class IsAdm(BaseBearer):
+    def has_permission(self, request, view, **kwargs) -> bool:
+        return super().has_permission(request, view, permissions=["ADM"])
+
+
+class IsPlayer(BaseBearer):
+    def has_permission(self, request, view, **kwargs) -> bool:
+        return super().has_permission(request, view, permissions=["PLAYER"])
+    
+class IsAnyUser(BaseBearer):
+    def has_permission(self, request, view, **kwargs) -> bool:
+        return super().has_permission(request, view, permissions=["PLAYER", "ADM"])
